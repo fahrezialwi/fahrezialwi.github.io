c46ec1f7668212f4ebee3c5e279d14c65a507931
fahrezialwi <fahrezialwi@gmail.com>
2019-09-29 09:45:15 +0700

    Initial commit

---
+++ js/magnific.js
@@ -0,0 +1,245 @@
+
+
+$('.photograph').magnificPopup({
+    items: [
+      {
+        src: '../album/1.jpg',
+        title: 'Great Mosque of Central Java, Semarang, Central Java, Indonesia'
+      },
+      {
+        src: '../album/2.jpg',
+        title: 'Amirul Mukminin Mosque, Makassar, South Sulawesi, Indonesia'
+      },
+      {
+        src: '../album/3.jpg',
+        title: 'Dusk in Jakarta, Indonesia'
+      },
+      {
+        src: '../album/4.jpg',
+        title: 'Dawn at Lempuyangan Station, Yogyakarta, Indonesia'
+      },
+      {
+        src: '../album/5.jpg',
+        title: 'Water Castle (Tamansari), Yogyakarta, Indonesia'
+      },
+      {
+        src: '../album/6.jpg',
+        title: 'Tanjung Kodok Beach Resort, Lamongan, East Java, Indonesia'
+      },
+      {
+        src: '../album/7.jpg',
+        title: 'Crepuscular Rays at Lake Toba, North Sumatra, Indonesia'
+      },{
+        src: '../album/8.jpg',
+        title: 'Dawn at Panambungan Island, Pangkep, South Sulawesi, Indonesia'
+      },
+      {
+        src: '../album/9.jpg',
+        title: 'Dusk at Bali Strait, Indonesia'
+      },{
+        src: '../album/10.jpg',
+        title: 'Sunset at Kuta Beach, Bali, Indonesia'
+      },
+      {
+        src: '../album/11.jpg',
+        title: 'Diamond Ring Phase of Total Solar Eclipse in March 9, 2016 at Balikpapan, East Kalimantan, Indonesia'
+      },
+      {
+        src: '../album/12.jpg',
+        title: 'Total Solar Eclipse in March 9, 2016 at Balikpapan, East Kalimantan, Indonesia'
+      },{
+        src: '../album/13.jpg',
+        title: 'Starry Night at Gili Labak Island, East Java, Indonesia'
+      },{
+        src: '../album/14.jpg',
+        title: 'Milky Way at Singkarak Lake, West Sumatra, Indonesia'
+      },
+      {
+        src: '../album/15.jpg',
+        title: 'Ranu Kumbolo Lake at Bromo Tengger Semeru National Park, East Java, Indonesia'
+      },
+      {
+        src: '../album/16.jpg',
+        title: 'Ranu Kumbolo Lake at Bromo Tengger Semeru National Park, East Java, Indonesia'
+      },
+      {
+        src: '../album/17.jpg',
+        title: 'Ranu Kumbolo Lake at Bromo Tengger Semeru National Park, East Java, Indonesia'
+      },
+      {
+        src: '../album/18.jpg',
+        title: 'Verbena Field at Bromo Tengger Semeru National Park, East Java, Indonesia'
+      },
+      {
+        src: '../album/19.jpg',
+        title: 'Underwater Photo at Gili Labak Island, East Java, Indonesia'
+      },
+      {
+        src: '../album/20.jpg',
+        title: 'Birds-Eye View of Kuala Lumpur, Malaysia'
+      },
+      {
+        src: '../album/21.jpg',
+        title: 'Skyline of Medina, Saudi Arabia'
+      },
+      {
+        src: '../album/22.jpg',
+        title: 'Miniature Effect of Bandung, West Java, Indonesia'
+      },
+      {
+        src: '../album/23.jpg',
+        title: 'Makkah Royal Clock Tower (Abraj Al-Bait), Mecca, Saudi Arabia'
+      },
+      {
+        src: '../album/24.jpg',
+        title: 'Aircraft Takes Off at Sultan Aji Muhammad Sulaiman Sepinggan Airport, Balikpapan, Indonesia'
+      },
+      {
+        src: '../album/25.jpg',
+        title: 'Green Bottle Fly (Lucilia Sericata) in Macro'
+      },
+      {
+        src: '../album/26.jpg',
+        title: 'Fire Ant (Solenopsis Geminata) in Macro'
+      },
+      {
+        src: '../album/27.jpg',
+        title: 'Sulphur-Crested Cockatoo (Cacatua Galerita)'
+      },
+      {
+        src: '../album/28.jpg',
+        title: 'Knowledge'
+      },
+      {
+        src: '../album/29.jpg',
+        title: 'Al-Hijra Street, Al-Misfalah, Mecca, Saudi Arabia'
+      },
+      {
+        src: '../album/30.jpg',
+        title: 'Traveler Life'
+      },
+
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
+
+				
+				
+							
+$('.videograph').magnificPopup({
+    items: [
+
+      {
+        src: 'https://www.youtube.com/watch?v=oyKIzSDBARU',
+        type: 'iframe'
+      },
+      {
+        src: 'https://www.youtube.com/watch?v=WYq9e6O0Kus',
+        type: 'iframe'
+      },
+      {
+        src: 'https://www.youtube.com/watch?v=3yM-t5_uJ5s',
+        type: 'iframe'
+      },
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
+
+								
+								
+											
+$('.design').magnificPopup({
+	
+    items: [
+      {
+        src: '../album/31.jpg',
+        title: 'Cover of Sinus Magazine (2013)'
+      },
+      {
+        src: '../album/32.jpg',
+        title: 'Inside of Sinus Magazine (2013)'
+      },
+      {
+        src: '../album/33.jpg',
+        title: 'Inside of Sinus Magazine (2013)'
+      },
+      {
+        src: '../album/34.jpg',
+        title: 'Inside of Sinus Magazine (2013)'
+      },
+      {
+        src: '../album/35.jpg',
+        title: 'Brand Label (2015)'
+      },
+      {
+        src: '../album/36.jpg',
+        title: 'Brand Label (2015)'
+      },
+      {
+        src: '../album/37.jpg',
+        title: 'Logo of Lessgen (2013)'
+      },
+      {
+        src: '../album/38.jpg',
+        title: 'Logo of Ngide Films (2014)'
+      },
+      {
+        src: '../album/39.jpg',
+        title: 'Logo of Zenith Production (2014)'
+      },
+      {
+        src: '../album/40.jpg',
+        title: 'Logo of Taman Waktu Films (2014)'
+      },
+      
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+    
+});
+
+												
+
+$('.webdesign').magnificPopup({
+    items: [
+      {
+        src: '../album/41.jpg',
+        title: 'Fahrezi Alwi Website, https://www.fahrezialwi.com (2017)'
+      },
+      {
+        src: '../album/42.jpg',
+        title: 'Lessgen Website, http://lessgen.blogspot.co.id, http://www.lessgen.com (2013)'
+      },
+      {
+        src: '../album/43.jpg',
+        title: 'Lessgen Website, http://lessgen.blogspot.co.id (2012)'
+      },
+      {
+        src: 'https://www.youtube.com/watch?v=J3fh7VbdglI',
+        type: 'iframe'
+      },
+      {
+        src: 'https://www.youtube.com/watch?v=RgXP1lNCr24',
+        type: 'iframe'
+      },
+      {
+        src: 'https://www.youtube.com/watch?v=Sz1sWgqFxu8',
+        type: 'iframe'
+      },
+
+
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
+			
